8d219af6deb1a215e40e51b6f52c6cff738958ff
Abhishek N <abhishekpes123@gmail.com>
2025-11-17 10:37:39 +0530

    first-commit

--- src/utils/Sendotp/sendOtp.ts
+++ src/utils/Sendotp/sendOtp.ts
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST, 
   port: 465,
-  secure: true,
+   secure: false,  // STARTTLS
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
