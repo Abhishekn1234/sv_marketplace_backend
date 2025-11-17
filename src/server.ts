07d2c712f996a139e42b718305b73b8c11d4ab8c
Abhishek N <abhishekpes123@gmail.com>
2025-11-17 11:10:48 +0530

    server configuration with CORS for specific origin

--- src/server.ts
+++ src/server.ts
@@ -16,9 +16,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  // origin[
-  //   "http://localhost:51730",
-  // ]
+  origin:[
+    "http://localhost:5173",
+  ]
 }));
 
 // Session configuration
