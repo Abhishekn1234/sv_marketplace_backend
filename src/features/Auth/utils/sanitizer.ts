9abf00b283023211ecfe52b250c9897d2d2186c5
Abhishek N <abhishekpes123@gmail.com>
2025-11-18 15:36:13 +0530

    full-code-commit backend

--- src/features/Auth/utils/sanitizer.ts
+++ src/features/Auth/utils/sanitizer.ts
@@ -1,7 +1,25 @@
 export const sanitizeUser = (user: any) => {
-  const { password, otp, __v, refreshToken, accessToken, ...rest } = user;
+  const { 
+    password, 
+    otp, 
+    __v, 
+    refreshToken, 
+    accessToken, 
+    resetPasswordToken,
+    resetPasswordExpires,
+    resetPasswordExpire,
+    lastLoginDate,
+    lastLoginTime,
+    logoutTime,
+    logoutDate,
+    duration,
+    LoginDate,
+    LoginTime,
+    ...rest 
+  } = user;
   return rest;
 };
+
 export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 export const phoneRegex = /^\+\d{1,3}\s?\d{10}$/;
 export const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*]{8,12}$/;
